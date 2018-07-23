053fc9885f45437208ccec014a0782e4d0f69dc3
termanator1128 <brennenstapleton@gmail.com>
2018-07-23 04:07:49 -0500

    Create Auth.js

---
+++ Auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+discordToken: 'NDcwMDA1MTcyMDYwMjkxMDkz.DjcU5A.cciVPgCCR1LPyeB8ON-SwfEh9H4',
+youtube: 'Youtube API key',
+trelloKey: 'Trello API key',
+trelloToken: 'Trello API token',
+dbPass: 'Database password',
+};
